1b7973245115aaee0eda609518b70dcee628a3e5
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-10-11 14:39:47 +0200

    Mysql with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from .config import Config
+from .models import db
 
 from .auth import auth
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
 
     app.register_blueprint(auth)
     return app
--- app/config.py
+++ app/config.py
@@ -1,2 +1,3 @@
 class Config:
     SECRET_KEY = 'SUPER SECRET'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost/test'
